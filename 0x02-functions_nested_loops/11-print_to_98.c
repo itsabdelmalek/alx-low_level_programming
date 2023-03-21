08238c339055811743017e120ec4cd8f89f5202d
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-21 12:09:01 -0700

    98 battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_to_98 - function that prints natural numbers to 98
+ * @n: number to start count from
+ * Return: dont return anything
+ */
+void print_to_98(int n)
+{
+	int;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
