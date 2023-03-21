c2c7daaa0f395bcb9337453ac0fa4c63d363480b
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-21 02:19:03 -0700

    i somtimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0 (sucess)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n);
+}
