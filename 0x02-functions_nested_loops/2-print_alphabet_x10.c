7408a687c62ff2b70b75f8a4e4bfb9454c22cc17
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-21 02:33:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; j <= 122; j++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n'):
+	}
+}
