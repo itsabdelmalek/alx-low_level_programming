ced006916281b2f5887332360abc280283f4a241
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-21 05:37:22 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,16 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			t = i * j;
-			if (j == 0)
+			if (j != 0)
 			{
-				_putchar(t + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (t < 10)
-				{
-					_putchar(' ');
-				}
+				_putchar((t / 10) + '0');
 				_putchar((t % 10) + '0');
 			}
 		}
