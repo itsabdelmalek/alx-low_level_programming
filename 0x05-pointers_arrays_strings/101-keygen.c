7816fce2aa7407f317f1d7a34cc75a1d2225ffbb
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-27 12:39:35 -0700

    dont hate the hacker hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = rand() % 94 + 33;
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("Generated password: %s\n", password);
+	return 0;
+}
