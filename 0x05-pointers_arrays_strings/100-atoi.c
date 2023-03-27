e297135690149d3b766d095df662d38d1b40065b
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-27 11:56:27 -0700

    Great leaders are willing to sacrifice the numbers to save the people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,20 +7,34 @@
  */
 int _atoi(char *s)
 {
-	int sub = 1, i = 0;
-	unsigned int result = 0;
+	int result = 0;
+	int sign = 1;
+	int found_number = 0;
 
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == '-')
-			sub *= -1;
-		i++;
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s == '+')
+		{
+			sign = 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_number = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (found_number)
+		{
+			break;
+		}
+		else if (*s != ' ')
+		{
+			return (0);
+		}
+		s++;
 	}
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-	{
-		result = (result * 10) + (s[i] - '0');
-		i++;
-	}
-	result *= sub;
-	return (sub);
+	return (sign * result);
 }
