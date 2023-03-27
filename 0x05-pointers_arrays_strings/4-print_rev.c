7ae6362bfa3717a1282210e5b2d9c90a25c80a58
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-27 06:46:28 -0700

    i can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,9 @@ void print_rev(char *s)
 {
 	int i, len;
 
-	for (len = 0; s[len] != '\0'; len++);
-	for (i = len -1; i >=0; i--)
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
