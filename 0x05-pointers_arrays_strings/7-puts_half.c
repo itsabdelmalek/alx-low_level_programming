d26714c0599fd79fcfc6fa67f66bd1b936c87733
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-27 07:25:16 -0700

    winning is only hald of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to print
+ *
+ * Return: no return
+ */
+
+void puts_hald(char *str)
+{
+	int len = 0, i;
+
+	while (str[len] != '\0')
+		len++;
+	i = len % 2 == 0 ? len / 2 : (len + 1) / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++
+	}
+}
