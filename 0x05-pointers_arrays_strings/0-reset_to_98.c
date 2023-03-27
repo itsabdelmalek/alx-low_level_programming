e9fbf1951cb37abeac6d69ba70f9eebaf6f94cd1
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-27 02:04:29 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an init to 98
+ * @n : the integer to be processed
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
