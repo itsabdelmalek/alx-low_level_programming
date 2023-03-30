c42367dd8489fad88d6c1b4eb1f84fbb2077abe9
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-30 01:36:08 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string to be encoded
+ *
+ * Return: a pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = .; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (ptr);
+}
