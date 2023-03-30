fbf3d96ef36681c694d43442368c7457f8724e94
abdelmalek <abdelmalek-ghost@live.fr>
2023-03-30 02:56:35 -0700

    noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,40 +3,43 @@
 
 /**
  * print_buffer - prints a buffer
- *
- * @b: buffer to print
- * @size: number of bytes to print
- *
- * Return: void
+ * @b: pointer to buffer
+ * @size: size of buffer
  */
+
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j, k;
 
-    if (size <= 0)
-        printf("\n");
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
-        for (j = i; j < i + 10; j++)
-        {
-            if (j < size)
-                printf("%02x", *(b + j));
-            else
-                printf("  ");
-            if (j % 2 != 0)
-                printf(" ");
-        }
-        for (j = i; j < i + 10; j++)
-        {
-            if (j >= size)
-                break;
-            if (*(b + j) >= 32 && *(b + j) <= 126)
-                printf("%c", *(b + j));
-            else
-                printf(".");
-        }
-        printf("\n");
-    }
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+				printf("%02x", *(b + j));
+			else
+				printf("  ");
+			if (j + 1 < size)
+				printf("%02x ", *(b + j + 1));
+			else
+				printf("   ");
+		}
+		for (k = i; k < i + 10; k++)
+		{
+			if (k >= size)
+				break;
+			if (*(b + k) >= ' ' && *(b + k) <= '~')
+				printf("%c", *(b + k));
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
 }
