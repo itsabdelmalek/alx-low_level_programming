deddbd3f55682df2133dc2c3f1570da5bedd2eec
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-05 03:25:22 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,25 +2,31 @@
 
 /**
  * is_prime_number - checks if a number is prime
- * @n: the number to check
+ * @n: integer to check
  *
- * Return: 1 if the number is prime, 0 otherwise
+ * Return: 1 if prime, 0 otherwise
  */
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
-	{
 		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
+	return (check_prime(n, 2));
+}
 
-	return (1);
+/**
+ * check_prime - helper function to check if number is prime
+ * @n: integer to check
+ * @div: current divisor to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int check_prime(int n, int div)
+{
+	if (n == div)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (check_prime(n, div + 1));
 }
