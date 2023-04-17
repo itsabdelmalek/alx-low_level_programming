f12054fcf61b38ca3573c73cbc42cf61063ad58e
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-17 02:18:36 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,15 @@
 #define DOG_H
 #define NULL ((void *)0)
 
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ *
+ * Description: a struct representing a dog with a name, age, and owner
+ */
+
 struct dog
 {
 	char *name;
