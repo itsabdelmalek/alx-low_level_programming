da27fc776251825e342e79ddb6ce5673f8591fab
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-17 02:29:54 -0700

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,6 @@ typedef struct dog
 } dog_t;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
