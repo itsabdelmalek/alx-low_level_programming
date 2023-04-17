d8e4483b2cf23a7546d8aaa7046d6f57b3168238
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-17 02:04:54 -0700

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog variable
+ * @d: pointer to the struct dog to print
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		char *name = d->name != NULL ? d->name : "(nil)";
+		float age = d->age;
+		char *owner = d->owner != NULL ? d->owner : "(nil)";
+		printf("Name: %s\n", name);
+		printf("Age: %.6f\n", age);
+		printf("Owner: %s\n", owner);
+	}
+}
