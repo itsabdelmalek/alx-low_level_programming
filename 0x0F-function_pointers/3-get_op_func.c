e26360c01b43422e9b304d98d7c57ef44786f41b
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-19 02:56:06 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,12 +18,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	i = 0;
-	while (ops[i].op)
+	while (*(ops[i].op) != '\0')
 	{
-		if (*(ops[i].op) == *s)
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
 }
