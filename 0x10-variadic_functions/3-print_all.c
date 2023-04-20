71374fcc489360632d5a9ce7e6c7e4ef6ba81f49
abdelmalek <abdelmalek-ghost@live.fr>
2023-04-20 15:22:00 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,38 +1,50 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
     va_list args;
+    int i = 0;
+    char *str;
+
     va_start(args, format);
 
-    char c;
-    int i;
-    float f;
-    char* s;
-    char* sep = "";
-
-    while (*format != '\0') {
-        if (*format == 'c') {
-            c = (char) va_arg(args, int);
-            printf("%s%c", sep, c);
-        } else if (*format == 'i') {
-            i = va_arg(args, int);
-            printf("%s%d", sep, i);
-        } else if (*format == 'f') {
-            f = (float) va_arg(args, double);
-            printf("%s%f", sep, f);
-        } else if (*format == 's') {
-            s = va_arg(args, char*);
-            if (s == NULL) {
-                printf("%s(nil)", sep);
-            } else {
-                printf("%s%s", sep, s);
-            }
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                printf("%c", va_arg(args, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(args, double));
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(nil)";
+                printf("%s", str);
+                break;
+            default:
+                i++;
+                continue;
         }
-        format++;
-        sep = ", ";
+
+        if (format[i + 1] != '\0')
+            printf(", ");
+
+        i++;
     }
+
     printf("\n");
     va_end(args);
 }
