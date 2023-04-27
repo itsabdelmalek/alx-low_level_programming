19484b1862308e82163ddf08bcd1e6006022312e
itsabdelmalek <itsabdelmalek@gmail.com>
2023-04-27 03:31:38 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,7 +19,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/* function prototypes */
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
