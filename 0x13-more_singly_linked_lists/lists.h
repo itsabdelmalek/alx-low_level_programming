eddab1c19d66e84b5ba0ab981954fd6f2275ee6c
itsabdelmalek <itsabdelmalek@gmail.com>
2023-04-28 01:40:37 -0700

    task 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 /* function prototype */
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
 
