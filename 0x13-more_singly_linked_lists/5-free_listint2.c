0ced05ef8043cd355af081d68d88d17df6a3738a
itsabdelmalek <itsabdelmalek@gmail.com>
2023-05-01 01:32:55 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current_node;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
-		current_node = *head;
-		*head = (*head)->next;
-		free(current_node);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
 }
