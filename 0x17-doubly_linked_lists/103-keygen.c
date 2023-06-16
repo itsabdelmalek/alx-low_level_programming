28c1c8dbbe081fc24eb19614aaaafcf5ad5e6442
abdelmalek <abdelmalek@gmail.com>
2023-06-16 02:20:00 -0700

    fix

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,43 +11,44 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char pass[7], *codex;
-	int length = strlen(argv[1]), i, tmp;
+	char pass[7], *keygen;
+	int length = strlen(argv[1]), i, temp;
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	keygen = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (length ^ 59) & 63;
-	pass[0] = codex[tmp];
+	temp = (length ^ 59) & 63;
+	pass[0] = keygen[temp];
 
-	tmp = 0;
+	temp = 0;
 	for (i = 0; i < length; i++)
-		tmp += argv[1][i];
-	pass[1] = codex[(tmp ^ 79) & 63];
+		temp += argv[1][i];
+	pass[1] = keygen[(temp ^ 79) & 63];
 
-	tmp = 1;
+	temp = 1;
 	for (i = 0; i < length; i++)
-		tmp *= argv[1][i];
-	pass[2] = codex[(tmp ^ 85) & 63];
+		temp *= argv[1][i];
+	pass[2] = keygen[(temp ^ 85) & 63];
 
-	tmp = 0;
+	temp = 0;
 	for (i = 0; i < length; i++)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][i] > temp)
+			temp = argv[1][i];
 	}
-	srand(tmp ^ 14);
-	pass[3] = codex[rand() & 63];
+	srand(temp ^ 14);
+	pass[3] = keygen[rand() & 63];
 
-	tmp = 0;
+	temp = 0;
 	for (i = 0; i < length; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	pass[4] = codex[(tmp ^ 239) & 63];
+		temp += (argv[1][i] * argv[1][i]);
+	pass[4] = keygen[(temp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	pass[5] = codex[(tmp ^ 229) & 63];
+		temp = rand();
+	pass[5] = keygen[(temp ^ 229) & 63];
 
 	pass[6] = '\0';
 	printf("%s", pass);
+
 	return (0);
 }
