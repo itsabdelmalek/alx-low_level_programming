38b3f007823f04a6b6c849b3eb26882421748ba6
abdelmalek <abdelmalek@gmail.com>
2023-07-06 05:40:18 -0700

    tasks

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (ht == NULL)
+		return;
+	/* Free the memory allocated for each node */
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	/* Free the memory allocated for the array and the hash table */
+	free(ht->array);
+	free(ht);
+}
